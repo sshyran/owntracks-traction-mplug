287a51890ff0e3640f7be1b3a88cd347bb3ca983
Jan-Piet Mens <jpmens@gmail.com>
2015-04-16 17:18:27 +0200

    Initial import from mosquitto-auth-plug, chopped down for Traction

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a "special copy" of [mosquitto-auth-plug](https://github.com/jpmens/mosquitto-auth-plug) with MySQL back-end only for OwnTracks.
---
+++ config.mk
@@ -0,0 +1,8 @@
+BACKEND_MYSQL ?= yes
+
+# Specify the path to the Mosquitto sources here
+MOSQUITTO_SRC = /Users/jpm/Auto/pubgit/MQTT/mosquitto/140/mosquitto/
+
+# Specify the path the OpenSSL here
+OPENSSLDIR = /usr/local/stow/openssl-1.0.1e/
+
